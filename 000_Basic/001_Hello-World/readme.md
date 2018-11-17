9009831a3f2307d380063adc0a714ca3537ca306
akagi <hoaiphuong.nguyen.vn@gmail.com>
2018-11-17 09:07:17 +0700

    Basic golang: "Hello World" and "Data Type" program.

---
+++ 000_Basic/001_Hello-World/readme.md
@@ -0,0 +1,7 @@
+Program : Hellow World
+
+Purpose: to print "hello world" message.
+
+How to run:
+- Use "go run" command: go run main.go, or
+- Build the program into binaries by command "go build main.go". Then, the main.exe file will be created. Finally, execute the main.exe file by command "./main"
---
+++ 000_Basic/002_Data-Type/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("go" + "lang")    // concatenate 2 strings
+	fmt.Println("1 + 2 =", 1+2)   // Integer
+	fmt.Println("7 / 3 =", 7.0/3) // floats
+	fmt.Println(true && false)    // boolean operator AND
+	fmt.Println(true || false)    // boolean operator OR
+	fmt.Println(!true)            // boolean operator NOT
+}
