ecec8317445e3347096f89ba1dd38814c23d58c6
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-07-02 11:40:52 +0700

    Defer function

--- 000_Basic/023_Defer/01_Defer-Function-Args/main.go
+++ 000_Basic/023_Defer/01_Defer-Function-Args/main.go
@@ -7,14 +7,29 @@ import (
 
 var wg sync.WaitGroup
 
-func main() {
+func passByValue(wg *sync.WaitGroup) {
 	wg.Add(1)
-	go func() {
-		i := 0
-		defer fmt.Println(i)
-		i++
-		wg.Done()
-		return
-	}()
+
+	i := 0
+	defer fmt.Println("Pass by value:", i)
+	i++
+
+	wg.Done()
+}
+
+func passByReference(wg *sync.WaitGroup) {
+	wg.Add(1)
+
+	i := new(int)
+	// fmt.Printf always pass by value
+	defer fmt.Printf("Pass by reference: %v\n", *i) // At this state, *i = 0
+	*i++
+
+	wg.Done()
+}
+
+func main() {
+	passByValue(&wg)
+	passByReference(&wg)
 	wg.Wait()
 }
