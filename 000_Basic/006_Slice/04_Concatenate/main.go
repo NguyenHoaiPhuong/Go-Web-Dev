42aa8848a1f9a583d1c64f378b07e8bf610f175d
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-10-02 15:10:28 +0700

    Concatenate a slice

--- 000_Basic/006_Slice/04_Concatenate/main.go
+++ 000_Basic/006_Slice/04_Concatenate/main.go
@@ -11,12 +11,23 @@ func main() {
 	}
 
 	a := arr[0:1]
-	fmt.Println("a:", a)
+	fmt.Println("a:", a, ", size:", len(a), ", cap:", cap(a))
 	fmt.Printf("address of a %p\n", &a)
 	fmt.Printf("a point to %p\n", a)
 
 	b := arr[2:]
-	fmt.Println("b:", b)
+	fmt.Println("b:", b, ", size:", len(b), ", cap:", cap(b))
+	fmt.Printf("address of b %p\n", &b)
+	fmt.Printf("b point to %p\n", b)
+
+	newArr := append(a, b...)
+	fmt.Println("newArr:", newArr, ", size:", len(newArr), ", cap:", cap(newArr))
+
+	fmt.Println("a:", a, ", size:", len(a), ", cap:", cap(a))
+	fmt.Printf("address of a %p\n", &a)
+	fmt.Printf("a point to %p\n", a)
+
+	fmt.Println("b:", b, ", size:", len(b), ", cap:", cap(b))
 	fmt.Printf("address of b %p\n", &b)
 	fmt.Printf("b point to %p\n", b)
 }
