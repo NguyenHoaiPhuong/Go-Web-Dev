ff3e9e459418586df9e4691d8053acb22156a2bb
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-10-15 11:22:57 +0700

    Go Build tags

---
+++ 008_Go-Build/hello/hello_debug.go
@@ -0,0 +1,9 @@
+// +build debug
+
+package hello
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Debug build")
+}
---
+++ 008_Go-Build/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/NguyenHoaiPhuong/Go-Web-Dev/008_Go-Build/hello"
+
+func main() {
+	hello.Hello()
+}
