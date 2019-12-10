57e2d549601d3ff0a300c889398ae8cd414217ad
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2019-12-10 16:20:36 +0700

    [redis] Add example 6

--- 015_Redis/main.go
+++ 015_Redis/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-redis/redis"
 )
@@ -31,6 +32,7 @@ func example1() {
 	// Output: PONG <nil>
 }
 
+// Set variable without expiration time
 func example2() {
 	err := client.Set("key", "value", 0).Err()
 	checkError(err)
@@ -51,9 +53,68 @@ func example2() {
 	// key2 does not exist
 }
 
+// Set variable with expiration time
+func example3() {
+	cmd := client.SetNX("myKey", "myValue", 10*time.Second)
+	fmt.Println(cmd)
+	fmt.Println(cmd.Args())
+	err := cmd.Err()
+	checkError(err)
+}
+
+// append a value into an existing variable
+func example4() {
+	cmd := client.Append("name", "Seo")
+	fmt.Println(cmd)
+	err := cmd.Err()
+	checkError(err)
+}
+
+// list
+func example5() {
+	listName := "task_queue"
+	intCmd := client.RPush(listName, "test_1", "test_2", "test_3")
+	fmt.Println(intCmd)
+	err := intCmd.Err()
+	checkError(err)
+
+	intCmd = client.LPush(listName, "test_4", "test_5")
+	fmt.Println(intCmd)
+	err = intCmd.Err()
+	checkError(err)
+
+	strCmd := client.LRange(listName, 0, -1)
+	values, err := strCmd.Result()
+	checkError(err)
+	for _, val := range values {
+		fmt.Println(val)
+	}
+}
+
+// remove variable from redis-server
+func example6() {
+	listName := "task_queue"
+	intCmd := client.Del(listName)
+	fmt.Println(intCmd)
+	res, err := intCmd.Result()
+	checkError(err)
+	fmt.Println("res:", res)
+
+	strCmd := client.LRange(listName, 0, -1)
+	values, err := strCmd.Result()
+	checkError(err)
+	for _, val := range values {
+		fmt.Println(val)
+	}
+}
+
 func main() {
-	// example1()
-	example2()
+	// example1() // ping the server
+	// example2() // set variable without expiration time
+	// example3() // set variable with expiration time
+	// example4() // append new value to an existing variable
+	// example5() // list
+	example6()
 }
 
 func checkError(err error) {
