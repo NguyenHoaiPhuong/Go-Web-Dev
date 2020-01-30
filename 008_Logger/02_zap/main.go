5c48eda7f3d6e62362aec6a3d9ec8ae6c7b5f9b3
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2020-01-30 10:15:53 +0700

    [zap] logger and sugar

--- 008_Logger/02_zap/main.go
+++ 008_Logger/02_zap/main.go
@@ -6,17 +6,49 @@ import (
 	"go.uber.org/zap"
 )
 
-func testT1()  {
+// sugar example
+func testT1() {
 	sugar := zap.NewExample().Sugar()
 	defer sugar.Sync()
 	sugar.Infow("failed to fetch URL",
-		"url", "http://example.com",	// key - value pair
-		"attempt", 3,					// key - value pair
-		"backoff", time.Second,			// key - value pair
+		"url", "http://example.com", // key - value pair
+		"attempt", 3, // key - value pair
+		"backoff", time.Second, // key - value pair
 	)
-	sugar.Infof("failed to fetch URL: %s", "http://example.com")	// formatted msg
+	sugar.Infof("failed to fetch URL: %s", "http://example.com") // formatted msg
+}
+
+// logger example
+func testT2() {
+	logger := zap.NewExample()
+	defer logger.Sync()
+	logger.Info("failed to fetch URL",
+		zap.String("url", "http://example.com"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+}
+
+// Converting logger and sugar
+func testT3() {
+	logger := zap.NewExample()
+	defer logger.Sync()
+	logger.Info("It is logger message",
+		zap.String("url", "http://example.com"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
+	sugar := logger.Sugar()
+	sugar.Infow("It is sugar message",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
 }
 
 func main() {
-	testT1()
+	// testT1()
+	// testT2()
+	testT3()
 }
