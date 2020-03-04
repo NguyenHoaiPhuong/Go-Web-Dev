635ad465dcc68dafbb74873d6b4e0057738bae2c
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-03-04 10:05:38 +0700

    [sendgrid] basic

--- 023_Email/01_Example/main.go
+++ 023_Email/01_Example/main.go
@@ -11,12 +11,20 @@ import (
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-func main() {
-	from := mail.NewEmail("Tokoin", "care@tokoin.io")
+func testSendMail1() {
+	fromName := "Tokoin"
+	fromEmail := "care@tokoin.io"
+	from := mail.NewEmail(fromName, fromEmail)
+
+	toName := "Akagi"
+	toEmail := "phuong.nguyen@tokoin.io"
+	to := mail.NewEmail(toName, toEmail)
+
 	subject := "Sending with SendGrid is Fun"
-	to := mail.NewEmail("Akagi", "phuong.nguyen@tokoin.io")
+
 	plainTextContent := "and easy to do anywhere, even with Go"
 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, err := client.Send(message)
@@ -27,4 +35,37 @@ func main() {
 		fmt.Println(response.Body)
 		fmt.Println(response.Headers)
 	}
+}
+
+func testSendMail2() {
+	fromName := "Akagi"
+	fromEmail := "phuong.nguyen@tokoin.io"
+	from := mail.NewEmail(fromName, fromEmail)
+
+	toName := "Nguyen Hoai Phuong"
+	toEmail := "hoaiphuong.nguyen.vn@gmail.com"
+	to := mail.NewEmail(toName, toEmail)
+
+	subject := "Sending email by sendgrid is so fun"
+
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	// client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	sendgridAPIKey := "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"
+	client := sendgrid.NewSendClient(sendgridAPIKey)
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
+}
+
+func main() {
+	// testSendMail1()
+	testSendMail2()
 }
