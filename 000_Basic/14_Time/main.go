eb0bf4d8072e01d315661cb6864aeef2ce0b931b
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-04-27 09:46:56 +0700

    [time] format

--- 000_Basic/14_Time/main.go
+++ 000_Basic/14_Time/main.go
@@ -5,12 +5,25 @@ import (
 	"time"
 )
 
-func main() {
-	t := time.Now()
-
+func toTimeString(t time.Time) {
 	str := ToTimeStringRFC3339(t)
 	fmt.Println(str)
 
 	str = ToTimeStringISO8601(t)
 	fmt.Println(str)
+}
+
+func formatTime(t time.Time) {
+	str := t.Format("2006-01-02 15:04:05")
+	fmt.Println(str)
+
+	str = t.Format("2006-01-02")
+	fmt.Println(str)
+}
+
+func main() {
+	t := time.Now()
+
+	toTimeString(t)
+	formatTime(t)
 }
